594493f091bbd00dd95516d5bae4ed3e04a61fa8
Shinsuke Nishio <nishio@densan-labs.net>
2021-07-13 03:01:44 +0900

    impl exit

--- src/core/repl.cr
+++ src/core/repl.cr
@@ -1,12 +1,24 @@
-module Musesql
-  module Core
-    class Repl
-      def print_prompt
-        print "> "
-      end
+class Musesql::Core::Repl
+  def print_prompt
+    print "> "
+  end
 
-      def read_input
-        gets
+  def read_input : String?
+    gets
+  end
+
+  def self.main
+    repl = self.new
+
+    while true
+      repl.print_prompt
+      user_input = repl.read_input
+      exit if user_input.nil?
+
+      if user_input.downcase == ".exit"
+        exit
+      else
+        puts "Unrecognized command %s" % user_input
       end
     end
   end
--- src/musesql.cr
+++ src/musesql.cr
@@ -2,18 +2,6 @@ require "./core/repl"
 
 module Musesql
   VERSION = "0.1.0"
-
-  def self.main
-    repl = Core::Repl.new
-
-    while true
-      repl.print_prompt
-      user_input = repl.read_input
-      exit if user_input.nil?
-
-      puts user_input
-    end
-  end
 end
 
-Musesql.main
+Musesql::Core::Repl.main
